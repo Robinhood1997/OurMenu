0de676ba2cdab83cdbd1ed24ebd69c04f1a71bbb
Manpreet Singh <58223888+Robinhood1997@users.noreply.github.com>
2022-02-06 17:18:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const menu = [{
         title: "Channa Dal",
         category: "Lunch",
         price: 50,
-        img: "./images/channa-dal.jpg",
+        img: "channa-dal.jpg",
         desc: `Channa Dal Is Our Speciality`,
     },
     {
@@ -11,7 +11,7 @@ const menu = [{
         title: "Dal Makhani",
         category: "lunch",
         price: 80,
-        img: "./images/maa-dal.jpg",
+        img: "maa-dal.jpg",
         desc: `Every Punjabi Favourite Daal is Daal Makhani `,
     },
     {
@@ -19,7 +19,7 @@ const menu = [{
         title: "Aloo Matar",
         category: "Lunch",
         price: 50,
-        img: "./images/aloo-matar.jpg",
+        img: "aloo-matar.jpg",
         desc: `Aloo Matar is a Punjabi Dish  .`,
     },
     {
@@ -27,7 +27,7 @@ const menu = [{
         title: "Kale Channe",
         category: "Lunch",
         price: 50,
-        img: "./images/kale-channe.jpg",
+        img: "kale-channe.jpg",
         desc: `Kale Channe Tadka Is Amazing `,
     },
     {
@@ -35,7 +35,7 @@ const menu = [{
         title: "Lasha Pratha",
         category: "lunch",
         price: 30,
-        img: "./images/lasha-pratha.jpg",
+        img: "lasha-pratha.jpg",
         desc: `Lasha Pratha With Daal is Best Combo `,
     },
     {
@@ -43,7 +43,7 @@ const menu = [{
         title: "Tandoori Roti",
         category: "Lunch",
         price: 5,
-        img: "./images/tandoori-roti.jpg",
+        img: "tandoori-roti.jpg",
         desc: `Tandoori Roti With Makhan Ahhhaaa!!`,
     },
     {
@@ -51,7 +51,7 @@ const menu = [{
         title: "Kari Pakoda",
         category: "Lunch",
         price: 50,
-        img: "./images/kari-pakoda.jpg",
+        img: "kari-pakoda.jpg",
         desc: `Kari Pakoda Is Second Favourite Punjabi Dish `,
     },
     {
@@ -59,7 +59,7 @@ const menu = [{
         title: "Masala Nutri Soya Chunks",
         category: "lunch",
         price: 50,
-        img: "./images/MasalaNutriSoyaChunks.jpg",
+        img: "MasalaNutriSoyaChunks.jpg",
         desc: `Masala Nutri Soya Chunks   `,
     },
     {
@@ -67,7 +67,7 @@ const menu = [{
         title: "Paneer Burji",
         category: "lunch",
         price: 120,
-        img: "./images/Paneer-Bhurji.jpg",
+        img: "Paneer-Bhurji.jpg",
         desc: `Paneer Bhurji Is Everyone Favourite Dish.`,
     },
     {
@@ -75,7 +75,7 @@ const menu = [{
         title: "Aloo Gobi",
         category: "lunch",
         price: 50,
-        img: "./images/aloo-gobi.jpg",
+        img: "aloo-gobi.jpg",
         desc: `Aloo Gobi with Tandoori Roti Ahaa!!.`,
     },
 ];
